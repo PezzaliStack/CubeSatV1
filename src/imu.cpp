a4ee5198596c5d195f4163f4929fa0c95d92773d
pezzaliapp <pezzalialessandro@gmail.com>
2025-06-01 15:55:55 +0200

    Create imu.cpp
    Create imu.cpp – start modularization for PayloadX

---
+++ src/imu.cpp
@@ -0,0 +1,14 @@
+// imu.cpp – PayloadX
+#include "imu.h"
+
+void initIMU() {
+    // TODO: implement IMU initialization
+}
+
+void readIMUData(sensorData &data) {
+    // TODO: implement IMU reading logic
+    // Example placeholder values:
+    data.ax = 0.0;
+    data.ay = 0.0;
+    data.az = 0.0;
+}
